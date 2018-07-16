5e78b38e4712ae6659c72900bc4c98cf8e287614
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-16 19:24:27 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
 
+
     redirect_to comment.post
   end
 
@@ -10,10 +11,6 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
-  end
-
-  def show
-
   end
 
 
