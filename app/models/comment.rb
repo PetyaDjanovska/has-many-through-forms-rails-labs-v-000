d1e2567b337c9bb7785ce0b07873a9748aa2de23
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-16 18:07:34 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+
+  def user_attributes=(user_attributes)
+    self.user = User.where(:id => user_attributes[:user_id]).first_or_create do |u|
+      u.username = user_attributes[:username]
+    end
+  end
+
 end
